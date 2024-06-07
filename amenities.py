f940372961572c4d88a5c1026533c75e1cc877d5
ivanpachecos <ivanpachecos@yahoo.com>
2024-06-07 14:41:18 -0500

    f- update function

--- amenities.py
+++ amenities.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
-
 from datetime import datetime
+import uuid
 
 
 class Amenities:
     """
     A class representing amenities.
     """
-    def __init__(self, id, name, place, create_time=None, update_time=None):
-        self.id = id
+    def __init__(self, id=None, name='', place=''):
+        self.id = str(uuid.uuid4()) if id is None else id
         self.name = name
         self.place = place
-        self.create_time = self.get_current_time(create_time)
-        self.update_time = self.get_current_time(update_time)
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
-    def get_current_time(self, time):
-        return time if time else datetime.now()
-
-    def update(self, new_name, new_place):
-        self.name = new_name
-        self.place = new_place
-        self.update_time = self.get_current_time(None)
+    def update(self, **kwargs):
+        for key, value in kwargs.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+        self.updated_at = datetime.now()
 
     def delete(self):
         pass
 
     def __str__(self):
-        return f"Amenity(ID: {self.id}, Name: {self.name}, Place: {self.place}, Created: {self.create_time}, Last Updated: {self.update_time})"
+        return f"Amenity(ID: {self.id}, Name: {self.name}, Place: {self.place}, Created: {self.created_at}, Last Updated: {self.updated_at})"
