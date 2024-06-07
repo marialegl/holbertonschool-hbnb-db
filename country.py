398ac863253f8f499eadb4f516495b22ca31fb3b
ivanpachecos <ivanpachecos@yahoo.com>
2024-06-06 19:49:17 -0500

    add files

---
+++ country.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+class Country:
+    """
+    A class representing a country.
+    """
+
+    def __init__(self, id, name, cities=None):
+        self.id = id
+        self.name = name
+        self.cities = cities if cities else []
+
+    def addCity(self, city):
+        self.cities.append(city)
+
+    def removeCity(self, city):
+        if city in self.cities:
+            self.cities.remove(city)
+        else:
+            print(f"{city} not found in cities")
+
+    def update(self, new_name):
+        self.name = new_name
+
+    def delete(self):
+        pass
+
+    def __str__(self):
+        cities_str = ', '.join(self.cities)
+        return f"Country(ID: {self.id}, Name: {self.name}, Cities: [{cities_str}])"
